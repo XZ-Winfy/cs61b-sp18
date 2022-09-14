3d07d98d97f3408d1d8cc014e177d3548c9b40e8
Winfy <lin120928053@gmail.com>
2022-09-14 00:56:10 -0700

    finish ArrayDeque

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,132 @@
+public class ArrayDeque<T> {
+    private T[] array;
+    private int size;
+    private int front;
+    private int end;
+    private int length;
+
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        size = 0;
+        front = 4;
+        end = 5;
+        length = 8;
+    }
+
+    public void addFirst(T item) {
+        if (size == length - 1) {
+            resize(size * 2);
+        }
+        array[front] = item;
+        front = minusone(front);
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        if (size == length - 1) {
+            resize(size * 2);
+        }
+        array[end] = item;
+        end = plusone(front);
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        int ptr = front;
+        while (ptr != end) {
+            System.out.println(array[ptr] + " ");
+            ptr = plusone(ptr);
+        }
+    }
+
+    public T removeFirst() {
+        if (length >= 16 && size/length <= 0.25) {
+            shrink();
+        }
+        if (size == 0) {
+            return null;
+        }
+        T ret = array[front];
+        front = plusone(front);
+        array[front] = null;
+        size -= 1;
+        return ret;
+    }
+
+    public T removeLast() {
+        if (length >= 16 && size/length <= 0.25) {
+            shrink();
+        }
+        if (size == 0) {
+            return null;
+        }
+        T ret = array[end];
+        end = minusone(end);
+        array[end] = null;
+        size -= 1;
+        return ret;
+    }
+
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        int ptr = front;
+        for (int i = 0; i < index; i++) {
+            ptr = plusone(ptr);
+        }
+        return array[ptr];
+    }
+
+    public int plusone(int i) {
+        if (i >= length-1) {
+            return 0;
+        }
+        return i + 1;
+    }
+
+    public int minusone(int i) {
+        if (i == 0) {
+            return length - 1;
+        }
+        return i - 1;
+    }
+
+    public void resize(int s) {
+        T[] a = (T[]) new Object[s];
+        int p1 = front;
+        int p2 = 0;
+        while (p1 != end) {
+            a[p2] = array[p1];
+            p1 = plusone(p1);
+            p2 += 1;
+        }
+        front = 0;
+        end = p2;
+        array = a;
+        length = s;
+    }
+
+    public void shrink() {
+        T[] a = (T[]) new Object[length/2];
+        int p1 = front;
+        int p2 = 0;
+        while (p1 != end) {
+            a[p2] = array[p1];
+            p1 = plusone(p1);
+            p2 += 1;
+        }
+        front = 0;
+        end = p2;
+        array = a;
+        length = length/2;
+    }
+}
